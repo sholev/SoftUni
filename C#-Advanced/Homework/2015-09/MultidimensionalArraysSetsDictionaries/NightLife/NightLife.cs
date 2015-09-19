8e96bd2fabcf5dcd24995b0b981d5c3cda9204d2
sholev <asen.sholev@gmail.com>
2015-09-19 13:57:12 +0300

    More Homework
    Multidimensional Arrays, Sets, dictionaries

---
+++ C#-Advanced/Homework/2015-09/MultidimensionalArraysSetsDictionaries/NightLife/NightLife.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class NightLife
+{
+    static void Main()
+    {
+        Dictionary<string, SortedDictionary<string, string[]>> performances = new Dictionary<string, SortedDictionary<string, string[]>>();
+        Dictionary<string, string[]> tempVenuePerformance = new Dictionary<string, string[]>();
+        string[] tempPerformances = new string[0];
+        string input = Console.ReadLine();
+        
+        while (input != "END")
+        {
+            string[] locationVenuePerformer = input.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (!performances.ContainsKey(locationVenuePerformer[0]))
+            {
+                tempVenuePerformance.Add(locationVenuePerformer[1], )
+                performances.Add(locationVenuePerformer[0], tempVenuePerformance);
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
