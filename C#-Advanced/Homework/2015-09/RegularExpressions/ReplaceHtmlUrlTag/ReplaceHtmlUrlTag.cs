bae1c9449fdd8ea4fbe26a007d03d5e7f47d42fa
sholev <sholev@users.noreply.github.com>
2015-09-24 22:10:20 +0300

    Series of letters and Replace HTML URL tag done.

---
+++ C#-Advanced/Homework/2015-09/RegularExpressions/ReplaceHtmlUrlTag/ReplaceHtmlUrlTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ReplaceHtmlUrlTag
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        // Match <a\s*href=["|'], get the first group $1 untill ["|']>, match $2 untill </a>
+        string matchPattern = @"<a\s*href=[""|'](.*?)[""|']>(.*?)<\/a>";
+        string replacement = @"[URL=$1]$2[/URL]";
+        Console.WriteLine(Regex.Replace(input, matchPattern, replacement));
+    }
+}
--- C#-Advanced/Homework/2015-09/RegularExpressions/SeriesOfLetters/SeriesOfLetters.cs
+++ C#-Advanced/Homework/2015-09/RegularExpressions/SeriesOfLetters/SeriesOfLetters.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 class SeriesOfLetters
 {
     static void Main()
     {
-
+        string input = Console.ReadLine();
+        // Get character (.) and \1* 
+        string matchPattern = @"(.)\1*";
+        string replacement = @"$1";
+        Console.WriteLine(Regex.Replace(input, matchPattern, replacement));
     }
 }
