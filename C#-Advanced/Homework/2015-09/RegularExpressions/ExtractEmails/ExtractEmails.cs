ed2fc17b2da9b7c9a9692bafb7921941b85d139c
sholev <sholev@users.noreply.github.com>
2015-09-25 15:30:49 +0300

    Exctact emails done, maybe?

--- C#-Advanced/Homework/2015-09/RegularExpressions/ExtractEmails/ExtractEmails.cs
+++ C#-Advanced/Homework/2015-09/RegularExpressions/ExtractEmails/ExtractEmails.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class ExtractEmails
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        // Need to get back to this and understand it better.
+        string matchPattern = @"(?<=\s|^)([^\W_][\w.-]*[^\W_])@([^\W]+[\w.-]+[^\W]+)";
 
+        // http://stackoverflow.com/questions/11416191/how-to-convert-matchcollection-to-string-array
+        string[] matches = Regex.Matches(input, matchPattern).Cast<Match>().Select(m => m.Value).ToArray();
+        Console.WriteLine(string.Join("\r\n", matches));
     }
 }
--- C#-Advanced/Homework/2015-09/RegularExpressions/SeriesOfLetters/SeriesOfLetters.cs
+++ C#-Advanced/Homework/2015-09/RegularExpressions/SeriesOfLetters/SeriesOfLetters.cs
@@ -6,7 +6,7 @@ class SeriesOfLetters
     static void Main()
     {
         string input = Console.ReadLine();
-        // Get character (.) and \1* 
+        // Get (.) and \1* will include the rest of the same
         string matchPattern = @"(.)\1*";
         string replacement = @"$1";
         Console.WriteLine(Regex.Replace(input, matchPattern, replacement));
