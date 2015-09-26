6324395dd5dcebf19595996ba312ffdaef03c175
sholev <sholev@users.noreply.github.com>
2015-09-26 08:11:15 +0300

    Sentence Extractor done

---
+++ C#-Advanced/Homework/2015-09/RegularExpressions/SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        string keyword = Console.ReadLine();
+        string input = Console.ReadLine();
+
+        string matchPattern = string.Format(@"(?<=\s|^)(?:\s)*([^?!.]*\b{0}\b[^!?.]*[!.?])", keyword);
+        // http://stackoverflow.com/questions/11416191/how-to-convert-matchcollection-to-string-array
+        string[] matches = Regex.Matches(input, matchPattern).Cast<Match>().Select(m => m.Value).ToArray();
+        Console.WriteLine(string.Join("\r\n", matches));
+    }
+}
