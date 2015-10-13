b505e37d3eab8a0329e27b1807393e6a55397c40
sholev <sholev@users.noreply.github.com>
2015-10-13 23:55:15 +0300

    Starting the homework.

---
+++ JavaFundamentals/Homework/Intro-Java/src/org/softuni/AssignVariables.java
@@ -0,0 +1,19 @@
+package org.softuni;
+
+public class AssignVariables {
+
+    public static void main(String[] args) {
+
+        byte descriptiveName = 127;
+        short anotherDescriptiveName = 32767;
+        int yayDescriptiveName = 32768;
+        long notAShortOne = 919827112351L;
+        int whatIsThisUsedFor = 2000000000;
+        char wellYouCantTell = 'c';
+        boolean byTheName = false;
+        float ingLikeABoat = 0.5f;
+        double theFloatingPower = 0.1234567891011;
+        String haventBeenThere = "Palo Alto, CA";
+    }
+}
+
