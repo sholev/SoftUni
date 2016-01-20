dd890daacaeafcbca2434f36a28d8d8d205d1d88
sholev <sholev@users.noreply.github.com>
2016-01-20 22:43:45 +0200

    Unit-Testing homework now has better code coverage.

--- HighQualityCode/Homework/Unit-Testing/CustomLinkedList/UnitTests/DynamicListUnitTest.cs
+++ HighQualityCode/Homework/Unit-Testing/CustomLinkedList/UnitTests/DynamicListUnitTest.cs
@@ -21,10 +21,33 @@
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "Empty list not throwing out of range exception.")]
-        public void Indexer_EmptyList_ThrowsException()
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Accessing unexisting index of empty list not throwing exception.")]
+        public void Indexer_EmptyListAccessingElement_ThrowsException()
         {
             var temp = this.dynamicList[0];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Saving an item to an unexisting index of empty list not throwing exception.")]
+        public void Indexer_EmptyListSettingElementAtIndex_ThrowsException()
+        {
+            this.dynamicList[0] = this.testDecimals[0];
+        }
+
+        [TestMethod]
+        public void Indexer_NonEmptyList_ReplaceElement()
+        {
+            this.dynamicList.Add(this.testDecimals[1]);
+            this.dynamicList.Add(this.testDecimals[2]);
+
+            this.dynamicList[0] = this.testDecimals[0];
+
+            Assert.AreEqual(
+                this.testDecimals[0],
+                this.dynamicList[0], 
+                "Replacing an element in a list is not working.");
         }
 
         [TestMethod]
@@ -34,8 +57,39 @@
 
             Assert.AreEqual(
                 this.testDecimals[2],
-                this.dynamicList[0], 
+                this.dynamicList[0],
                 "The element at [0] position is incorrect.");
+        }
+
+        [TestMethod]
+        public void Count_ListIsInitialized_ListIsEmpty()
+        {
+            int count = this.dynamicList.Count;
+
+            Assert.AreEqual(0, count, "A list without any entries should have 0 count.");
+        }
+
+        [TestMethod]
+        public void Count_ListIsInitializedAndNotEmpty_ListIsEmpty()
+        {
+            this.dynamicList.Add(this.testDecimals[1]);
+            this.dynamicList.Add(this.testDecimals[2]);
+            this.dynamicList.RemoveAt(0);
+            this.dynamicList.RemoveAt(0);
+            int count = this.dynamicList.Count;
+
+            Assert.AreEqual(0, count, "List with all elements should be empty.");
+        }
+
+        [TestMethod]
+        public void Count_ListHasElements_ListIsNotEmpty()
+        {
+            this.dynamicList.Add(this.testDecimals[0]);
+            this.dynamicList.Add(this.testDecimals[1]);
+            this.dynamicList.Add(this.testDecimals[2]);
+            int count = this.dynamicList.Count;
+
+            Assert.AreEqual(3, count, "The list should have 3 entries in it.");
         }
 
         [TestMethod]
@@ -97,6 +151,19 @@
         }
 
         [TestMethod]
+        public void Remove_DecimalNumber_NumberDoesNotExist()
+        {
+            this.dynamicList.Add(this.testDecimals[2]);
+            this.dynamicList.Add(this.testDecimals[0]);
+
+            var result = this.dynamicList.Remove(this.testDecimals[1]);
+            Assert.AreEqual(
+                -1, 
+                result,
+                "The result of trying to remove an unexisting item is wrong.");
+        }
+
+        [TestMethod]
         public void Remove_DecimalNumber_NumberIsNotRemoved()
         {
             this.dynamicList.Add(this.testDecimals[0]);
@@ -110,7 +177,8 @@
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException), "Method didn't throw exception on invalid index.")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Method didn't throw exception on invalid index.")]
         public void RemoveAt_DecimalNumber_ThrowsException()
         {
             this.dynamicList.Add(this.testDecimals[0]);
@@ -136,7 +204,10 @@
             this.dynamicList.Add(this.testDecimals[1]);
 
             this.dynamicList.RemoveAt(0);
-            Assert.AreEqual(this.testDecimals[1], this.dynamicList[0], "The entry next to the removed one is not moved.");
+            Assert.AreEqual(
+                this.testDecimals[1],
+                this.dynamicList[0],
+                "The entry next to the removed one is not moved.");
         }
 
         [TestMethod]
@@ -146,7 +217,10 @@
             this.dynamicList.Add(this.testDecimals[1]);
 
             var removedEntry = this.dynamicList.RemoveAt(0);
-            Assert.AreEqual(false, this.dynamicList.Contains(this.testDecimals[0]), "The removed entry is still present.");
+            Assert.AreEqual(
+                false,
+                this.dynamicList.Contains(this.testDecimals[0]),
+                "The removed entry is still present.");
         }
     }
 }
