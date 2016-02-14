aeeea3d62b5b65d1eabc5a09288d440930bce68d
sholev <sholev@users.noreply.github.com>
2016-02-14 14:58:45 +0200

    Camera, follow scripts changes.

--- Unity3D/Basics/Assets/Scripts/CameraFollow.cs
+++ Unity3D/Basics/Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,17 @@
     {
         public Transform Target;
 
+        private Rigidbody TargetRigidbody;
+
         [Range(0, 1)]
         public float FollowSpeed;
-
-        public float MinDistance;
 
         private Vector3 offset;
 
         private void Start()
         {
             this.offset = this.transform.position - this.Target.transform.position;
+            this.TargetRigidbody = this.Target.GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
@@ -25,17 +26,11 @@
             {
                 return;
             }
-            
-            float xDistance = this.transform.position.x - this.Target.position.x;
-            float zDistance = this.transform.position.z - this.Target.position.z;
 
-            if (Mathf.Abs(zDistance) > this.MinDistance || Mathf.Abs(xDistance) > this.MinDistance)
-            {
-                ////Debug.Log("xDistance: " + xDistance);
-                ////Debug.Log("zDistance: " + zDistance);
-                var direction = this.Target.transform.position + this.offset;
-                this.transform.position = Vector3.Lerp(this.transform.position, direction, this.FollowSpeed);
-            }
+            ////Debug.Log("xDistance: " + xDistance);
+            ////Debug.Log("zDistance: " + zDistance);
+            var direction = this.Target.transform.position + this.offset;
+            this.transform.position = Vector3.Lerp(this.transform.position, direction, this.FollowSpeed);
         }
     }
 }
--- Unity3D/Basics/Assets/Scripts/FollowTarget.cs
+++ Unity3D/Basics/Assets/Scripts/FollowTarget.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+
+using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
@@ -10,31 +12,54 @@ public class FollowTarget : MonoBehaviour
 
     public float MinDistance;
 
+    public float StopDrag;
+
     private Rigidbody rigidBody;
-    
+
+    private Stopwatch cooldownTimer;
+
     void Start()
     {
         this.rigidBody = this.GetComponent<Rigidbody>();
+        this.cooldownTimer = new Stopwatch();
+        this.cooldownTimer.Start();
     }
 
     void LateUpdate()
     {
-        if (this.Target != null)
+        if (this.Target == null)
         {
-            var direction = this.Target.transform.position - this.transform.position;
-
-            if (Vector3.Distance(this.transform.position, this.Target.position) > this.MinDistance)
-            {
-                ////Debug.Log(this.name + ": " + direction);
-                this.rigidBody.drag = 0;
-
-                // Adding fore is more fun than transpose. :D
-                this.rigidBody.AddForce(Vector3.ClampMagnitude(direction, this.SpeedLimit) * this.FollowMultiplier);
-            }
-            if (Vector3.Distance(this.transform.position, this.Target.position) < this.MinDistance)
-            {
-                this.rigidBody.drag = 5;
-            }
+            return;
         }
+
+        var direction = this.Target.transform.position - this.transform.position;
+
+        if (Vector3.Distance(this.transform.position, this.Target.position) > this.MinDistance)
+        {
+            this.rigidBody.drag = 0;
+
+            // Adding force is more fun than transpose. :D
+            this.rigidBody.AddForce(Vector3.ClampMagnitude(direction, this.SpeedLimit) * this.FollowMultiplier);
+        }
+
+        if (Vector3.Distance(this.transform.position, this.Target.position) < this.MinDistance)
+        {
+            this.rigidBody.drag = 5;
+        }
+
+        // Most likely out of camera view, no need to keep going in the same direction. 
+        // Remove the force with drag so the next force will be applied towards the target direction.
+        if (Vector3.Distance(this.transform.position, this.Target.position) > 100 && this.cooldownTimer.ElapsedTicks > 500)
+        {
+            this.cooldownTimer.Reset();
+            this.rigidBody.drag = this.StopDrag;
+        }
+    }
+
+    // Getting out of bounds, adding force tends to do that.
+    void OnTriggerExit(Collider other)
+    {
+        this.rigidBody.drag = this.StopDrag;
+        this.transform.position = this.Target.position;
     }
 }
