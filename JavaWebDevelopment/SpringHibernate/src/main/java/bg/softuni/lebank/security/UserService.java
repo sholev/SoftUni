e1c99db3aef9763b92b543ec116a5ae53011e6a2
sholev <sholev@users.noreply.github.com>
2016-04-07 22:07:50 +0300

    Clean up.

--- JavaWebDevelopment/SpringHibernate/src/main/java/bg/softuni/lebank/security/UserService.java
+++ JavaWebDevelopment/SpringHibernate/src/main/java/bg/softuni/lebank/security/UserService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import bg.softuni.lebank.interfaces.UserStorage;
 
+@Service
 public class UserService implements UserDetailsService {
 
 	@Autowired
