c74ce188351189802465eca2195163b277baef31
sholev <sholev@users.noreply.github.com>
2016-05-16 22:19:02 +0300

    New directory structure.

---
+++ SoftUni-3.0/Advanced-C#-May-2016/StacksAndQueues/Stacks-ReverseNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Stacks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class ReverseNumbers
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+
+            Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(n => numbers.Push(n));
+
+            while (numbers.Count > 0)
+            {
+                Console.Write($"{numbers.Pop()} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
