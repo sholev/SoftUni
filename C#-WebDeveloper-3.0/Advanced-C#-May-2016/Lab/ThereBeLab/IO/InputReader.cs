a08f31f38fabef45ed44a9e4cd30a6e96b146798
sholev <sholev@users.noreply.github.com>
2016-06-02 12:44:53 +0300

    Working on an InputReader

---
+++ C#-WebDeveloper-3.0/Advanced-C#-May-2016/Lab/ThereBeLab/IO/InputReader.cs
@@ -0,0 +1,65 @@
+﻿namespace ThereBeLab.IO
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class InputReader
+    {
+        private const string EndCommand = "quit";
+
+        public static void StartReadingCommands()
+        {
+            string input = ReadCommand();
+            while (input != EndCommand)
+            {
+                input = ReadCommand();
+            }
+        }
+
+        public static void InterpretCommand(string input)
+        {
+            string[] parameters = Regex.Split("\\s+", input);
+            string command = parameters[0];
+
+            switch (command)
+            {
+                case "open":
+                    break;
+                case "mkdir":
+                    break;
+                case "ls":
+                    break;
+                case "cmp":
+                    break;
+                case "cdRel":
+                    break;
+                case "cdAbs":
+                    break;
+                case "readDb":
+                    break;
+                case "help":
+                    break;
+                case "filter":
+                    break;
+                case "order":
+                    break;
+                case "decOrder":
+                    break;
+                case "download":
+                    break;
+                case "downloadAsync":
+                    break;
+                default:
+                    DisplayInvalidCommandMessage(input);
+            }
+        }
+
+        private static string ReadCommand()
+        {
+            OutputWriter.WriteMessage($"{SessionData.CurrentPath}> ");
+            string input = Console.ReadLine()?.Trim();
+
+            return input;
+        }
+    }
+}
